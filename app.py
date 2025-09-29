16b88844a26bdb8e8c1fe0844631c9fe4bc520b5
Arina Goloubitskaya <arinaogo@gmail.com>
2025-09-29 19:21:34 +0300

    added file reading

--- app.py
+++ app.py
@@ -1,13 +1,45 @@
-#!pip install gradio
-import gradio as gra
+import io
+from typing import Annotated
+
+import joblib
+import numpy as np
+import pandas as pd
+from fastapi import FastAPI, File, HTTPException, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+
+from base_logger import logger
 
 
-def user_greeting(name):
-    return "Hi, " + name + ", welcome to your first Gradio application!"
+app = FastAPI()
+
+# CORS middleware allowing all origins and methods
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-# define gradio interface and other parameters
-app = gra.Interface(fn=user_greeting, inputs="text", outputs="text")
-app.launch()
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    logger.info("call predict")
 
-# gradio app.py
+    # Validation of file type
+    if not file.filename.endswith('.txt'):
+        raise HTTPException(400, "File must be txt")
+
+    content = await file.read()
+
+    logger.info(f"File content {content}")
+
+    return
+
+
+# Health check endpoint
+@app.get("/")
+async def root():
+    return {"message": "LLM Model API is running!"}
